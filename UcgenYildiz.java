94edef935b28e57538764f987408ae489690cd21
Derya Günay <58901502+DeryaGny@users.noreply.github.com>
2022-01-14 21:23:19 +0300

    UcgenYildiz

---
+++ UcgenYildiz.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class UcgenYildiz {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Lütfen sayı giriniz :");
+        n = input.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >=0 ; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+    }
+
+
