a4759aa6a91edf8a813f29fab65f5904fd0ec205
Derya Günay <58901502+DeryaGny@users.noreply.github.com>
2022-04-18 22:04:03 +0300

    BharfYazdirma

---
+++ BsayisiYazdir.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class BsayisiYazdir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[][] letter = new String[7][4];
+
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else if ( i == 6){
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    }
+
+
